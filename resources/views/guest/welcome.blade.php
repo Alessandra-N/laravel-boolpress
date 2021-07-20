0360f1ae38e012e6f4ac7c861ef59e96e93f5a5f
Alessandra-N <nardella.alessandra1987@gmail.com>
2021-07-20 17:41:14 +0200

    cambiata rotta admin

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -75,6 +75,8 @@
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
                         @endif
+                    @else
+                    
                     @endauth
                 </div>
             @endif
