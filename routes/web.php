7ab0ab5d42a6998854c83d915d7768ad2e5f8836
Alessandra-N <nardella.alessandra1987@gmail.com>
2021-07-20 15:01:20 +0200

    creata cartella admin

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
-Route::resource('products', ProductController::class)->only('index', 'show');
+Route::resource('products', ProductController::class)->only(['index', 'show']);
 
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin')->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
     Route::resource('articles', ArticleController::class);
 });
